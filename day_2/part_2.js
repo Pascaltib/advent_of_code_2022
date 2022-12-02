28978e0933eea3d22706f3df1f1587fb81abe330
Pascal Tiberghien <pascaltib@gmail.com>
2022-12-02 14:00:25 +0100

    day 2 part 2 done

--- day_2/part_2.js
+++ day_2/part_2.js
@@ -1,3 +1,38 @@
 var fs = require('fs');
 
 let data = fs.readFileSync('data.txt', 'utf8');
+
+// X = lose Y = draw Z = win
+// If you win it is 6 points tie is 3 loss is 0
+// Rock is 1
+// Paper 2
+// Scissors 3
+const gameLogic = {
+  A: { youLose: 3, youWin: 8, tie: 4 },
+  B: { youLose: 1, youWin: 9, tie: 5 },
+  C: { youLose: 2, youWin: 7, tie: 6 },
+}
+const gamePoints = (opponentPlay, myPlay) => {
+
+  let points = 0
+  if (myPlay === "X") {
+    points += gameLogic[opponentPlay].youLose
+  } else if (myPlay === "Y") {
+    points += gameLogic[opponentPlay].tie
+  } else {
+    points += gameLogic[opponentPlay].youWin
+  }
+  return points
+}
+
+
+
+data = data.split("\n")
+console.log((data))
+let score = 0;
+for (let i = 0; i < data.length; i++) {
+  const element = data[i];
+  const arr = element.split(" ")
+  score += gamePoints(arr[0], arr[1])
+}
+console.log(score)
